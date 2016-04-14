eb739c91bcd9ea128e6f700ca20dd8c09c0461d9
Chris Johnson <johnsonc.88@gmail.com>
2016-04-14 11:50:06 -0700

    minor changes

--- tabs.js
+++ tabs.js
@@ -22,9 +22,9 @@ var Tab = React.createClass({
 
 // make an array to populate the Tabs
 
-var tablist = [
+var tabList = [
 	{ 'id': 1, 'name': 'Mike', 'url': 'http://www.teenagemutantninjaturtles.com/wp-content/uploads/2012/04/Ninja-Turtles-Michelangelo-cover-photo.jpg'},
     { 'id': 2, 'name': 'Donnie', 'url': 'http://static.tumblr.com/79d6283ba71c3827c0f74b3e969b8d28/wvxcw4r/4o1mzhb0e/tumblr_static_tmnt_donatello_1600.jpg' },
     { 'id': 3, 'name': 'Raph', 'url': 'http://crimsonmonkey.com/files/media/DaveOMac/2014/Top5-Leo-3.jpg' },
     { 'id': 4, 'name': 'Leo', 'url': 'http://crimsonmonkey.com/files/media/DaveOMac/2014/Top5-Leo-1.jpg' }
-]
+];
